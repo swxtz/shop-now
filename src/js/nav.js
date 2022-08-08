73bea64c7afbba296bdf5edbf48289240abf75fc
Gustavo <alf4r6@gmail.com>
2022-08-08 04:04:36 -0300

    mobile

---
+++ src/js/nav.js
@@ -0,0 +1,27 @@
+const onClickAll = document.getElementById('all')
+
+function onClickNav() {  
+    all.classList.remove('selected')
+
+    if() {
+      
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Quando clicar 
+// adicionar class selected
+// quando trocar 
+// remover class selected e
+// adicionar no atual
